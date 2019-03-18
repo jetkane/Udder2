8685b13f189259023fa405f82563d70c39bbf083
Jetkane <JetkaneChang@fih-foxconn.com>
2019-03-18 18:06:21 +0800

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by JetKane on 2019/3/18.
+//  Copyright © 2019 JetKane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
